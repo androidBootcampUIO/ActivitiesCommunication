b1e8adef937c40b1a5f0bb6c18940720e687b1d5
Sebastian Puente <sebas7pr@gmail.com>
2017-10-24 20:25:05 -0500

    Agrega comunicacion entre los Activity para enviar y recibir el mensaje ingresado por el usuario

--- app/src/main/java/androidbootcamp/uio/activitiescommunication/MainActivity.java
+++ app/src/main/java/androidbootcamp/uio/activitiescommunication/MainActivity.java
@@ -1,13 +1,27 @@
 package androidbootcamp.uio.activitiescommunication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    public static final String EXTRA_MESSAGE = "androidbootcamp.uio.activitiescommunication.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        EditText editText = findViewById(R.id.editText);
+        String message = editText.getText().toString();
+
+        Intent intent = new Intent(this, ReceivingActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
--- app/src/main/java/androidbootcamp/uio/activitiescommunication/ReceivingActivity.java
+++ app/src/main/java/androidbootcamp/uio/activitiescommunication/ReceivingActivity.java
@@ -1,7 +1,9 @@
 package androidbootcamp.uio.activitiescommunication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReceivingActivity extends Activity {
 
@@ -9,5 +11,11 @@ public class ReceivingActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receiving);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.messageTextView);
+        textView.setText(message);
     }
 }
